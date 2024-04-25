ac70c1e8b79450a47aba8c093db3dba31cd50cac
Ying Her <yher.prime@gmail.com>
2024-04-24 21:43:21 -0500

    ProductList component created with comments

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+function ProductList() {
+  return (
+    <>
+      <h1>Product List</h1>
+      {/* TO-DO: Create a table with the Product List */}
+      {/* Map the Product List  */}
+      {/* Will need to make sure API get request is set up in the server side route -- Nate is currently working on this */}
+      {/*  */}
+    </>
+  );
+}
+
+export default ProductList;
