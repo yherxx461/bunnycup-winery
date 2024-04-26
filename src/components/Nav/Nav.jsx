5254008f0fef4b5f50fdd5e90173740ab2779729
Gabriel Regalado <regs0404@gmail.com>
2024-04-26 18:57:33 -0400

    added navigation link to the order summary page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -32,6 +32,10 @@ function Nav() {
               Info Page
             </Link>
 
+            <Link className="navLink" to="/orderSummary">
+              Order Summary
+            </Link>
+
             <LogOutButton className="navLink" />
           </>
         )}
