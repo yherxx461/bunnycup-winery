9d910df3a98d469fcf480a72fcfd96cdd4206a10
Ying Her <yher.prime@gmail.com>
2024-05-01 21:30:57 -0500

    added mapping function to the item list to make sure the ProductList is rendering correctly

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -10,6 +10,8 @@ import {
 } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+// import './LandingPage.css';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 // CUSTOM COMPONENTS
@@ -25,8 +27,8 @@ function LandingPage() {
   }, []);
   const inventory = useSelector((store) => store.inventory.inventoryList);
   const imageList = useSelector((store) => store.inventory.imageList);
-  console.log('THIS IS THE INVENTORY', inventory);
-  console.log('THIS IS THE WINE LIST', imageList);
+  // console.log('THIS IS THE INVENTORY', inventory);
+  // console.log('THIS IS THE WINE LIST', imageList);
 
   const handleAddToCart = (sku) => {
     console.log('Add to cart:', sku);
@@ -49,7 +51,6 @@ function LandingPage() {
         {/* Map the Product List  */}
         {/* Will need to make sure API get request is set up in the server side route */}
         <TableContainer
-          key={item.sku}
           component={Paper}
           align="center"
           style={{ font: 'Montserrat' }}
@@ -94,32 +95,49 @@ function LandingPage() {
               </TableRow>
             </TableHead>
             <TableBody>
-              <TableRow
-                key={item.sku}
-                className="product-list"
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell align="center">Product Image</TableCell>
-                <TableCell align="center" sx={{}}>
-                  {item.name}
-                </TableCell>
-                <TableCell align="center">{item.sku}</TableCell>
-                <TableCell align="center">{item.teaser}</TableCell>
-                <TableCell align="center">{item.category}</TableCell>
-                <TableCell align="center">{item.inv_level}</TableCell>
-                <TableCell align="center">{item.retail_price}</TableCell>
-                <TableCell align="center">{item.quantity}</TableCell>
-                <TableCell align="center">
-                  <Button
-                    size="small"
-                    variant=""
-                    type="button"
-                    onClick={() => handleAddToCart(item.sku)}
+              {inventory.map((item) => (
+                <TableRow
+                  key={item.sku}
+                  className="product-list"
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="center">
+                    <img
+                      // filter through the wine.sku images to match inventory.sku of the inventory table
+                      src={
+                        imageList.filter((imageItem) => {
+                          return imageItem.sku === item.sku;
+                        })[0].image
+                      }
+                      alt={item.name}
+                      style={{ width: '100px', height: 'auto' }}
+                    />
+                  </TableCell>
+                  <TableCell align="center">{item.name}</TableCell>
+                  <TableCell align="center">{item.sku}</TableCell>
+                  <TableCell
+                    align="center"
+                    style={{ whiteSpace: 'normal', wordWrap: 'break-words' }}
                   >
-                    Add to Cart
-                  </Button>
-                </TableCell>
-              </TableRow>
+                    {item.teaser}
+                  </TableCell>
+                  <TableCell align="center">{item.category}</TableCell>
+                  <TableCell align="center">{item.inv_level}</TableCell>
+                  <TableCell align="center">{item.retail_price}</TableCell>
+                  <TableCell align="center">{item.quantity}</TableCell>
+                  <TableCell align="center">
+                    <Button
+                      size="small"
+                      variant="contained"
+                      style={{ backgroundColor: 'white', color: 'black' }}
+                      type="button"
+                      onClick={() => handleAddToCart(item.sku)}
+                    >
+                      Add to Cart
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </TableContainer>
