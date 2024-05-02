58328a0df6086d32ed1d5600aefafc66333d17d0
Gabriel Regalado <regs0404@gmail.com>
2024-05-01 20:07:48 -0400

    committing change before pull request

---
+++ src/components/OrderHistory/OrderHistory.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './OrderHistory.css';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
+
+function OrderHistory() {
+  //Setting up state for sample data to list
+  const data = [
+    { Date: '04/30/2024', Total: '$100.50' },
+    { Date: '05/01/2024', Total: '$85.45' },
+    { Date: '05/02/2024', Total: '$95.89' },
+  ];
+  console.log('Data object', data);
+
+  //Functions for view and reorder button
+  const viewHandle = (event) => {
+    console.log('In View Handle');
+  };
+
+  const reorderHandle = (event) => {
+    console.log('In Reorder Handle');
+  };
+
+  return (
+    <>
+      <div className="header">
+        <div>
+          <h1>Order History</h1>
+        </div>
+      </div>
+      <main className="main">
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={3}>
+            {data.map((item, index) => (
+              <Grid xs={8} key={index} className="listItem">
+                <p>
+                  <div className="dateTotal">
+                    <Stack spacing={2}>
+                      <p>Date: {item.Date}</p>
+                      <p>Total: {item.Total}</p>
+                    </Stack>
+                  </div>
+                  <div className="button">
+                    <Button
+                      variant="contained"
+                      onClick={viewHandle}
+                      sx={{ marginLeft: '500px' }}>
+                      View
+                    </Button>
+                    <Button
+                      variant="contained"
+                      sx={{ marginLeft: '15px' }}
+                      onClick={reorderHandle}>
+                      Reorder
+                    </Button>
+                  </div>
+                </p>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </main>
+    </>
+  );
+}
+
+export default OrderHistory;
---
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './OrderSummary.css';
+
+function OrderSummary() {
+  //dispatch hook
+  const dispatch = useDispatch();
+  const reducer = useSelector((store) => store.reducer);
+
+  /*//Formatting Date
+  const formatDate = (newDate) => {
+    // //This splits string into substrings/array
+    // const splitDate = newDate.split('T');
+    // // console.log('new date object', splitDate);
+    // return splitDate[0];
+
+    //new date formatting
+    const date = new Date(newDate);
+    // console.log(date);
+    const formattedDate = date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'numeric',
+      day: 'numeric',
+      year: 'numeric',
+    });
+    // console.log(formattedDate);
+    const splitDate = formattedDate.split(',');
+    // console.log(splitDate);
+    // const weekday = splitDate[0];
+    const monthDay = splitDate[1];
+    const splitMonthDay = monthDay.split(' ');
+    // console.log('splitMonthDay', splitMonthDay);
+    const newDateFormat = splitMonthDay[1];
+    // console.log('newDateFormat', newDateFormat);
+    const replaceNewDateFormat = newDateFormat.replace(/\//g, '-');
+    // console.log('replaceNewDateFormat', replaceNewDateFormat);
+
+    return `${replaceNewDateFormat}`;
+  };*/
+
+  // Fetch orders on component mount
+  useEffect(() => {
+    dispatch({ type: 'FETCH_REDUCER' });
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="header">
+        <h1>Order #XXXXXX-1</h1>
+      </div>
+      <div className="customerInfo">
+        {/*To Do: Retailer info includes Name, Address, contact info */}
+        <h3>Order Summary </h3>
+        <p className="date">Date:</p>
+        Retailer Name <br />
+        Address <br />
+        Phone <br />
+        Email <br />
+      </div>
+
+      {/*To Do: set up Table with MUI */}
+      <div className="container">
+        <table className="orderTable">
+          <thead>
+            {/*To Do: Table headers needed are item, description, quantity, price, amount */}
+            <tr style={{ borderTop: '1px solid' }}>
+              <th style={{ borderBottom: '1px solid' }}>Item</th>
+              <th style={{ borderBottom: '1px solid' }}>Description</th>
+              <th style={{ borderBottom: '1px solid' }}>Quantity</th>
+              <th style={{ borderBottom: '1px solid' }}>Price</th>
+              <th style={{ borderBottom: '1px solid' }}>Amount</th>
+            </tr>
+          </thead>
+          <tbody style={{ borderBottom: '1px solid' }}>
+            <tr>
+              <td>1.</td>
+              <td>Product Description 1</td>
+              <td>12</td>
+              <td>Unit Price</td>
+              <td>$120.00</td>
+            </tr>
+            <tr>
+              <td>2.</td>
+              <td>Product Description 2</td>
+              <td>24</td>
+              <td>Unit Price</td>
+              <td>$200.00</td>
+            </tr>
+            <tr>
+              <td>3.</td>
+              <td>Product Description 3</td>
+              <td>12</td>
+              <td>Unit Price</td>
+              <td>$150.00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="total">
+        <p>Total: $</p>
+      </div>
+    </>
+  );
+}
+
+export default OrderSummary;
