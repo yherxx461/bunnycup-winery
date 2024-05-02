07538d568ab15658debc515f5814f6d35a4a1199
Ariel Rodriguez <r.ariel94@gmail.com>
2024-05-02 00:34:18 -0500

    added some styling and picture to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import { Box } from "@mui/material";
+
+import noraisedpinkies from "../../../public/images/noraisedpinkies.png";
 
 //This is a super simple footer element, that attributes the application as being a part of Bunnycup Winery
 
 function Footer() {
-  return <footer>&copy; Bunnycup Winery</footer>;
+  return (
+    <div>
+      <Box
+        className="footerItem"
+        style={{
+          backgroundImage: `url(${noraisedpinkies})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "150px",
+          height: "25vh",
+          color: "black",
+        }}
+      >
+        <footer>&copy; Bunnycup Winery</footer>
+      </Box>
+    </div>
+  );
 }
 
 export default Footer;
