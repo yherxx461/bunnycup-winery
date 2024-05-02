41d4275da631ac4240e6acb7ec125f289d78c36d
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-05-01 20:21:05 -0400

    separated client and admin registration.

--- server/routes/clients.router.js
+++ server/routes/clients.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const router = express.Router();
 
@@ -22,7 +23,34 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
-  // POST route code here
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
+  const client = {name: req.body.name,
+                  street: req.body.street,
+                  city: req.body.city,
+                  state: req.body.state, 
+                  zip: req.body.zip,
+                  discount: req.body.discount,
+                  payment: req.body.payment};
+
+  const clientText = `WITH "ins1" as (
+    INSERT INTO "user" ("email", "password", "access_level")
+    VALUES ($1, $2, $3)
+    RETURNING "id"),
+    "ins2" AS (
+    INSERT INTO "clients" ("user_id", "name", "email", "discount", "payment_type")
+    SELECT "id", $4, $5, $6, $7 FROM "ins1"
+    RETURNING "id")
+  INSERT INTO "client_address" ("client_id", "street", "city", "state", "zip")
+  SELECT "id", $8, $9, $10, $11 FROM "ins2";`;
+
+  pool.query(clientText, [username, password, 1, client.name, username, client.discount, client.payment, client.street, client.city, client.state, client.zip])
+    .then((result) => {
+      res.sendStatus(200)
+    })
+    .catch((error) => {
+      console.log('Retailer registration failed: ', error)
+      res.sendStatus(500)})
 });
 
 module.exports = router;
