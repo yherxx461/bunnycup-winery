140188424c89ff819944a166649140652dbe8303
Ying Her <yher.prime@gmail.com>
2024-05-05 23:23:28 -0500

    Commented out TableContainer. Call in reducers for cart (cartWines), imageList, client, and clientInfo to make sure all the cart information is displayed correctly and successfully. Pending or to-do: Place order function, remove item, and make sure subtotal doesn't display 'NaN'

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,7 +3,7 @@ import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,
+  // TableContainer,
   TableHead,
   TableRow,
   Button,
@@ -14,106 +14,149 @@ import './ShoppingCart.css';
 
 function ShoppingCart() {
   const dispatch = useDispatch();
-  const cart = useSelector((store) => store.cart);
-  // const client = useSelector((store) => store.clients);
-  const clientInfo = useSelector((store) => store.clientDetails);
+  const imageList = useSelector((store) => store.inventory.imageList);
+  const cart = useSelector((store) => store.orders.cartWines);
+  const client = useSelector((store) => store.clients);
+  const clientInfo = useSelector((store) => store.orders.cartInfo);
 
-  console.log('THIS IS THE CART', cart);
+  console.log('THIS IS THE ITEMS ADDED TO CART', cart);
   console.log('THIS IS THE CLIENT INFO', clientInfo);
+  console.log('THIS IS THE CLIENT', client);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_CART' });
+    console.log(
+      'Cart Items:',
+      cart.map((item) => ({
+        name: item.name,
+        quantity: item.quantity,
+        retail_price: item.retail_price,
+        quantityType: typeof item.quantity,
+        priceType: typeof item.retail_price,
+      }))
+    );
+  }, [cart]);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_IMAGES' });
+    dispatch({ type: 'GET_CLIENT_ORDERS' });
     dispatch({ type: 'FETCH_CLIENT_DETAILS' });
-    // dispatch({ type: 'FETCH_CLIENTS' });
+    dispatch({ type: 'FETCH_CLIENTS' });
   }, [dispatch]);
 
   // Place Order function
   const placeOrder = () => {
-    console.log('Placing an order:,');
-    dispatch({ type: 'PLACE_ORDER', payload: { cart, clientInfo } });
+    console.log('Placing an order:', cart, clientInfo, client);
+    dispatch({ type: 'PLACE_ORDER', payload: { cart, clientInfo, client } });
   };
+
+  // Remove Item from Cart
+  const handleRemoveItem = (sku) => {
+    console.log('Removing item from Cart:', sku);
+    dispatch({ type: 'REMOVE_ITEM_FROM_CART', payload: sku });
+  };
+
+  // Calculation subtotal
 
   return (
     <div>
       <h1 className="cart-list-title" align="center">
         Shopping Cart
       </h1>
-      {/* {client && ( */}
-      <div className="retailer-address">
-        <h3>{clientInfo.name}</h3>
-        <p>{clientInfo.street}</p>
-        <p>
-          {clientInfo.city}, {clientInfo.state} {clientInfo.zip}
-        </p>
-      </div>
-      {/* )} */}
+      {client && ( // If there is a client, show the information below
+        <div className="retailer-info-address">
+          <h3>{clientInfo.name}</h3>
+          <p>{clientInfo.street}</p>
+          <p>
+            {clientInfo.city}, {clientInfo.state} {clientInfo.zip}
+          </p>
+        </div>
+      )}
       <div className="default-payment">
         <h3>Payment Method</h3>
-        <p>Default Payment</p>
+        <p>{client?.payment_type || 'Default Payment'}</p>
       </div>
       <Button
         size="small"
         variant="outlined"
         type="button"
-        onClick={() => placeOrder(item.sku)}
+        onClick={placeOrder}
         style={{ marginRight: '3rem' }}
       >
         Place Order
       </Button>
-      {/* {(item) => ( */}
-      <TableContainer
-        // key={item.sku}
-        component={Paper}
-        align="center"
-        // justifyContent="center"
-      >
-        <Table sx={{ maxWidth: 900 }} arial-label="simple table" align="center">
-          <TableHead>
+      <Table sx={{ maxWidth: 900 }} arial-label="simple table" align="center">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">
+              <h3>Product Image</h3>
+            </TableCell>
+            <TableCell align="center">
+              <h3>Product Name</h3>
+            </TableCell>
+            <TableCell align="center">
+              <h3>SKU #</h3>
+            </TableCell>
+            <TableCell align="center">
+              <h3>Retail Price</h3>
+            </TableCell>
+            <TableCell align="center">
+              <h3>Quantity</h3>
+              {/* <p>(# of Bottles)</p> */}
+            </TableCell>
+            <TableCell align="center">
+              <h3>Subtotal</h3>
+            </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* Conditional rendering: If there's no items in the cart, show the 'Your cart is empty' message */}
+          {cart.length === 0 ? (
             <TableRow>
-              <TableCell align="center">
-                <h3>Product Image</h3>
-              </TableCell>
-              <TableCell align="center">
-                <h3>Product Name</h3>
-              </TableCell>
-              <TableCell align="center">
-                <h3>SKU #</h3>
-              </TableCell>
-              <TableCell align="center">
-                <h3>Retail Price</h3>
-              </TableCell>
-              <TableCell align="center">
-                <h3>Quantity</h3>
-                {/* <p>(# of Bottles)</p> */}
-              </TableCell>
-              <TableCell align="center">
-                <h3>Subtotal</h3>
+              <TableCell colSpan={7} align="center">
+                Your cart is empty
               </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {cart &&
-              cart.map((item) => (
-                <TableRow
-                  className="product-list"
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell align="center"></TableCell>
-                  <TableCell align="center">{item.name}</TableCell>
-                  <TableCell align="center">{item.sku}</TableCell>
-                  <TableCell align="center">{item.retail_price}</TableCell>
-                  <TableCell align="center">{item.quantity}</TableCell>
-                  <TableCell align="center">Subtotal</TableCell>
-                  <TableCell align="center">
-                    <Button variant="outlined" size="small">
-                      Remove
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          ) : (
+            cart.map((item) => (
+              <TableRow
+                className="product-list"
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="center">
+                  <img
+                    // filter through the wine.sku images to match inventory.sku of the inventory table
+                    src={
+                      imageList.filter((item) => {
+                        return item.sku === item.sku;
+                      })[0]?.image
+                    }
+                    alt={item.name}
+                    style={{ width: '100px', height: 'auto' }}
+                  />
+                </TableCell>
+                <TableCell align="center">{item.product_name}</TableCell>
+                <TableCell align="center">{item.wine_sku}</TableCell>
+                <TableCell align="center">{item.unit_price}</TableCell>
+                <TableCell align="center">{item.number_bottles}</TableCell>
+                <TableCell align="center">
+                  {(item.number_bottles * item.unit_price).toFixed(2)}
+                </TableCell>
+                <TableCell align="center">
+                  <Button
+                    variant="outlined"
+                    size="small"
+                    onClick={() => handleRemoveItem(item.sku)}
+                  >
+                    Remove
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+      {/* </TableContainer> */}
     </div>
   );
 }
