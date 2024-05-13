f900b34d6f5ffe655dfe73b0c6db64364f10e7a9
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-05-13 11:57:14 -0500

    Added text property to email router

--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -7,7 +7,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post('/', async (req, res) => {
     try {
-        const orderedWines = req.body.wines.map((wine) => (`<li>${wine.sku}__ ${wine.quantity}</li>`))
+        const orderedWinesHTML = req.body.wines.map((wine) => (`<li>${wine.sku}__ ${wine.quantity}</li>`))
+        const orderedWinesText = req.body.wines.map((wine) => (`${wine.sku}__ ${wine.quantity}`))
         const discountedPrice = (
             Number(req.body.cost) -
             Number(req.body.cost) * (req.body.discount / 100)
@@ -17,7 +18,17 @@ router.post('/', async (req, res) => {
             to: 'nateglewwe@gmail.com',
             from: 'nateglewwe@gmail.com', // Use the email address or domain you verified in SendGrid account
             subject: `New Order #${req.body.order_id} from ${req.body.client_name}`,
-            text: 'Test text here', // We're not using this text property, not really sure what it does
+            text:
+                `New Order from ${req.body.client_name}:
+                Order__ #${req.body.order_id}
+                Date placed__ ${req.body.date}
+                Total cost before discount__ $${req.body.cost}
+                Discount percentage__ ${req.body.discount}%
+                Discounted price__ $${discountedPrice}
+                Payment type__ ${req.body.payment_type}
+                Wine quantities ordered:
+                ${orderedWinesText}
+                `, // Text we want in body of email, this is for email services that use Plaintext
             html:
                 `<p>New Order from ${req.body.client_name}:</p><br>
                  <p>Order__ #${req.body.order_id}</p>
@@ -28,9 +39,9 @@ router.post('/', async (req, res) => {
                  <p>Payment type__ ${req.body.payment_type}</p>
                  <p>Wine quantities ordered:</p>
                  <ul>
-                    ${orderedWines}
+                    ${orderedWinesHTML}
                  </ul>
-                 `, // This is the text we want to put in the body of the email
+                 `, // Text we want in body of email, this is for email services that use HTML
           };
           console.log('THIS IS THE ORDER PAYLOAD:', req.body);
 
